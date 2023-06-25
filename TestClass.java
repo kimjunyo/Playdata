da50015811b70507513a363b03fa1315e748ed19
kimjunyo <119035407+kimjunyo@users.noreply.github.com>
2023-06-25 19:22:07 +0900

    Add files via upload

---
+++ TestClass.java
@@ -0,0 +1,17 @@
+package com.kjy;
+/*
+ */
+/**
+ * 
+ * @author USER
+ *
+ */
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕");
+		
+	}
+
+}
