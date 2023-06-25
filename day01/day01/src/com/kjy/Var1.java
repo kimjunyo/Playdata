8db0410ddb0c573651424efef1e2148410e40ab9
kimjunyo <119035407+kimjunyo@users.noreply.github.com>
2023-06-25 21:14:42 +0900

    Add files via upload

---
+++ day01/day01/src/com/kjy/Var1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.kjy;
+
+/**
+ * @author 김준영
+ *
+ */
+public class Var1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
